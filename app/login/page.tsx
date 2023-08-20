6022b8019c223998f19c4cc40bc31dc5328cd988
DemetriuGabriel <demetriugabriel2011@hotmail.com>
2023-08-20 18:32:22 -0300

    change login page style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,30 +15,28 @@ export default function Login() {
     <main className="flex flex-col items-center justify-center h-screen">
         <div className="flex flex-col items-center justify-center">
             <img src="https://i.ibb.co/hxN3P63/Logo.png" alt="Logo UFPE" />
-            <h2>UFPE Map</h2>
+            <h2 className='text-white text-xl font-bold m-4'>UFPE Map</h2>
         </div>
-        <div className="login-container text-white">
-          <h1 className='font-semibold'>Bem vindo ao <strong className='text-[#0D615E] font-semibold'>LOGIN</strong></h1>
+        <div className=" w-96 h- flex flex-col bg-white bg-opacity-30 items-center text-white rounded-3xl p-6">
+          <h1 className='text-xl font-semibold mb-8'>Bem vindo ao <strong className='text-[#0D615E] font-bold'>LOGIN</strong></h1>
           <form action="#" method="POST" onSubmit={handleLogin}></form>
-            <div className="input-field">
+            <div className="flex flex-col justify-center">
               <div className='mb-4'>
-                <label className='block text-white font-semibold'>Digite seu e-mail</label>
+                <label className='block text-white font-semibold mb-2'>Digite seu e-mail</label>
                 <input 
-                  //id="email" 
                   type="email"
-                  className="w-full border rounded p-2" 
-                  placeholder="Digite seu e-mail"
+                  className="w-80 border text-gray-600 rounded-lg p-2" 
+                  placeholder="E-mail"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)} 
                 />
               </div>
               <div>
-                <label className='block text-white font-semibold'>Digite sua senha</label>
+                <label className='block text-white font-semibold mb-2'>Digite sua senha</label>
                 <input 
-                  //id="senha" 
                   type="text"
-                  className='w-full border rounded p-2'
-                  placeholder="Digite sua senha"
+                  className='w-80 border text-gray-600 rounded-lg p-2'
+                  placeholder="Senha"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)} 
                 />
@@ -46,11 +44,11 @@ export default function Login() {
             </div>
             <button
               type='submit'
-              className='w-ful bg-btn-login text-white font-semibold p-2 rounded'
+              className='w-ful bg-btn-login text-white font-semibold p-1 px-6 mt-8 rounded-lg'
             >Acessar</button>
           <form/>
-          <button>Esqueci minha senha</button>
-          <button>Criar novo usuário</button>
+          <button className='self-start mt-8'>Esqueci minha senha</button>
+          <button className='self-start mt-4'>Criar novo usuário</button>
         </div>
     </main>
   )
