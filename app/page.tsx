f583ba27fa0aa30db17198004a3eb56a90f6b895
Luana Brito <lccb@cin.ufpe.br>
2023-08-28 21:44:13 -0300

    pagina de novo usuário em andamento

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import LoginPage from './login/page'
+import NewUserPage from './new-user/newUser'
 
-export default function Home() {
+export function Home() {
   return (
     <div className="min-h-screen">
       <LoginPage />
+    </div>
+  )
+}
+
+export default function NewUser() {
+  return (
+    <div>
+      <NewUserPage />
     </div>
   )
 }
